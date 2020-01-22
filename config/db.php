f93e797ae4816f20ba244f0ac7d575d0f0cb7888
Catalin Teodorescu <catalin.teodorescu@teamextension.ro>
2020-01-22 03:10:11 +0200

    more changes

--- config/db.php
+++ config/db.php
@@ -16,5 +16,5 @@ return [
     'database' => getenv('DB_DATABASE'),
     'schema' => getenv('DB_SCHEMA'),
     'tablePrefix' => getenv('DB_TABLE_PREFIX'),
-    'port' => getenv('DB_PORT')
+    'port' => getenv('DB_PORT'),
 ];
