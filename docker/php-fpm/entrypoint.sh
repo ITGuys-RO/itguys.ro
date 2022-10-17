850713ce8ab2322a1a11ba4376fa0e04bb5daa11
Catalin Teodorescu <dustfeather@gmail.com>
2022-10-17 18:34:39 +0300

    added docker env

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 composer install --optimize-autoloader --apcu-autoloader --no-interaction --no-progress --prefer-dist
-php /var/www/artisan storage:link
-php /var/www/artisan migrate
-
+php /var/www/craft migrate/all
+php /var/www/craft project-config/apply
+php /var/www/craft clear-caches/all
+php /var/www/craft gc --delete-all-trashed=1
 /usr/local/sbin/php-fpm -D
