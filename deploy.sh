a5b6156b8c93834fee30a96627e44e14c023abcf
Catalin Teodorescu <dustfeather@gmail.com>
2022-10-18 04:09:02 +0300

    self-hosted gihub actions runner

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-git pull
+git checkout .
+git pull --rebase
 composer install --no-dev --no-interaction --prefer-dist
-php craft migrate/all
-php craft project-config/apply
-php craft clear-caches/all
-php craft gc --delete-all-trashed=1
+php craft migrate/all --interactive=0
+php craft project-config/apply --interactive=0
+php craft clear-caches/all --interactive=0
+php craft gc --delete-all-trashed=1 --interactive=0
 chmod 777 . -R
 chown www-data:www-data . -R
